6ee2127b53baf652e4d4f8dfbf584c03425a9fbd
kcollins1233 <150693203+kcollins1233@users.noreply.github.com>
2024-04-17 00:56:57 +0100

    Add files via upload

--- app/src/main/python/socket_server.py
+++ app/src/main/python/socket_server.py
@@ -1,25 +1,26 @@
 import socket
 import struct
 from PIL import Image
+from colorise import colorise
 import io
 
 def receive_image():
 #Create a socket object
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_in = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Define the host on which you want to connect
     # host = socket.gethostname()
 
     # Define the port on which you want to connect
-    port = 8080
+    port_in = 8080
 
     # connect to the server on local computer
-    s.bind(("192.168.0.197", port))
-    s.listen(4)
-    print("Listening on port: ", port)
+    socket_in.bind(("192.168.0.197", port_in))
+    socket_in.listen(4)
+    print("Listening on port: ", port_in)
 
     while True:
-        client, addr = s.accept()
+        client, addr = socket_in.accept()
         print("Connection from: ", addr)
         
         # Receive the size of the byte array
@@ -34,15 +35,15 @@ def receive_image():
         image = Image.open(io.BytesIO(byte_data))
         image.save("received_image.bmp")
         client.close()
-        s = 0
-        send_image()
+        colorise("received_image.bmp")
+        # send_image()
         
 def send_image():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    port = 8080
-    s.connect(("192.168.0.199", port))
-    s.listen(4)
-    print("Listening on port: ", port)
+    socket_out = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port_out = 8181
+    socket_out.connect(("192.168.0.199", port_out))
+    socket_out.listen(4)
+    print("Listening on port: ", port_out)
     
     # Load the image
     image = Image.open("received_image.bmp")
@@ -55,16 +56,13 @@ def send_image():
     
     # Send the size of the byte array
     length = struct.pack('!i', len(byte_data))
-    s.send(length)
+    socket_out.send(length)
     
     # Send the byte array
-    s.send(byte_data)
+    socket_out.send(byte_data)
     
     # Close the connection
-    socket.close()
-    
+    socket_out.close()
+
 if __name__ == "__main__":
     receive_image()
-    
-
-    
